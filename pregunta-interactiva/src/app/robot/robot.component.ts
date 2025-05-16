afcd02c62739eea140f9904e58e74079eba27e80
Eth4nSP <eth4n.gerl@gmail.com>
2025-05-16 02:43:33 -0400

    Aniimaciones de botones

--- pregunta-interactiva/src/app/robot/robot.component.ts
+++ pregunta-interactiva/src/app/robot/robot.component.ts
@@ -13,18 +13,52 @@ export class RobotComponent {
   y = 0;
   paso = 40;
   comandos: string[] = [];
+  comando_img: string []= ["⬅️","➡️","⬆️","⬇️"];
+  comandos_img: string [] = [];
+  ejecutando = false;
 
-  procesarComando(comando: string) {
-    // Guardar el comando
-    if (this.comandos.length < 20) {
-    this.comandos.push(comando);
-    } else {
-    // Opcional: reemplazar el más antiguo (FIFO) o ignorar si ya hay 20
-    this.comandos.shift(); // Elimina el primero
-    this.comandos.push(comando); // Agrega el nuevo al final
+  agregarComando(comando: string) {
+    if (this.comandos.length < 20 && !this.ejecutando) {
+      switch (comando){
+        case "up":
+          this.comandos_img.push(this.comando_img[2]);
+          break;
+        case "down":
+          this.comandos_img.push(this.comando_img[3]);
+          break;
+        case "left":
+          this.comandos_img.push(this.comando_img[0]);
+          break;
+        case "right":
+          this.comandos_img.push(this.comando_img[1]);
+          break;
+      }
+      this.comandos.push(comando);
     }
+  }
 
-    // Procesar movimiento
+  ejecutarComandos() {
+    if (this.ejecutando || this.comandos.length === 0) return;
+
+    this.ejecutando = true;
+    let i = 0;
+
+    const intervalo = setInterval(() => {
+      if (i >= this.comandos.length) {
+        clearInterval(intervalo);
+        this.ejecutando = false;
+        //this.comandos.splice(0, this.comandos.length); //limpia de forma correcta
+         this.comandos = []; //limpia de forma basica
+        return;
+      }
+
+      this.mover(this.comandos[i]);
+      i++;
+      this.comandos_img.shift();
+    }, 350); // cada 350ms se mueve uno
+  }
+
+  mover(comando: string) {
     if (comando === 'up') this.y = Math.max(0, this.y - this.paso);
     else if (comando === 'down') this.y = Math.min(260, this.y + this.paso);
     else if (comando === 'left') this.x = Math.max(0, this.x - this.paso);
